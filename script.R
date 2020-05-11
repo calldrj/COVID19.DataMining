3a1556c088c7c0308f8b4a8e8fce6f2b6c88763b
Tuan Nguyen <efranchisor@gmail.com>
2020-05-10 21:10:00 -0700

    3rd commit

--- script.R
+++ script.R
@@ -1,15 +1,19 @@
 # **** Data Mining  **** #
 # 1. Webpage's url for data of CV19 in the US mining
 cv19_url <- 'https://en.wikipedia.org/w/index.php?title=2020_coronavirus_outbreak_in_the_United_States&oldid=944107102'
+
 # 2. Scrap the webpage then store in an object
 library(rvest)      # read_html
 library(magrittr)   # pipe %>%
 cv19_wiki <- read_html(cv19_url)
+
 # 3. Save the raw data of CV19 in the US in a table
 cv19_raw <- cv19_wiki %>% html_nodes('table') %>% .[[5]] %>% html_table(fill=TRUE)
+
 # 4. Visible review the structure and content of the raw data table
 str(cv19_raw)
 View(cv19_raw)
+
 # 5. Save the data of confirmed, deaths, and recovered cases from the raw table
 # Set border of the states' data table (top, bottom [the last updated row], left, right) 
 # Row #93 corresponds with date 05/09/2020 
@@ -19,11 +23,13 @@ t_st <- c(2,93,1,56)
 
 cv19_us <- cv19_raw[c(t_us[1]:t_us[2]), c(t_us[3]:t_us[4])]
 cv19_st <- cv19_raw[c(t_st[1]:t_st[2]), c(t_st[3]:t_st[4])]
+
 # 6. Visible review the structure and content of the saved data table
 str(cv19_us)
 View(cv19_us)
 str(cv19_st)
 View(cv19_st)
+
 # 7. Remove some irrelevant rows (17, 18, 50, 51), then reset the row index of the data table
 cv19_us <- cv19_us[-c(17:18, 50:51, 82:83), ]
 cv19_st <- cv19_st[-c(17:18, 50:51, 82:83), ]
@@ -65,16 +71,19 @@ cv19_st[ , col][cv19_st[ , col]==''] <- 0
 # 10. Test sum of a column, but it fails due to improper data type
 sum(cv19_us$Confirmed)
 sapply(cv19_us[1:4], class)
+
 # 11. Convert data in columns, except Date of both tables to number
 col <- c(2:7)
 cv19_us[ , col] <- apply(cv19_us[ , col], 2, function(x) as.numeric(x))
 col <- c(2:56)
 cv19_st[ , col] <- apply(cv19_st[ , col], 2, function(x) as.numeric(x))
+
 # 12. Retest column data type
 sapply(cv19_us[1:4], class)
 str(cv19_st)
 sum(cv19_us$Deaths)
 sum(cv19_st$CA)
+
 # 13. Fix and format the Date column in proper form for manipulations
 d <- as.Date('01/21/2020', format='%m/%d/%Y')
 # Last update date
@@ -84,14 +93,17 @@ Date <- c(d + i)
 View(Date)
 cv19_us$Date <- Date
 cv19_st$Date <- Date
+
 # 14. Double check column data types for both tables
 str(cv19_us)
 str(cv19_st)
+
 # 15. Convert 2 tables to csv data frame, then double check
 us.csv <- as.data.frame(cv19_us)
 View(us.csv)
 state.csv <- as.data.frame(cv19_st)
 View(state.csv)
+
 # 16. Save the csv dataset to a local drive
 write.csv(us.csv, 'us.csv', row.names=FALSE)
 write.csv(state.csv, 'state.csv', row.names=FALSE)
