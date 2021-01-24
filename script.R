0273bbfa3269efe85517f59e19f91fac84954320
Tuan Nguyen <efranchisor@gmail.com>
2021-01-23 21:51:14 -0800

    last update on Jan. 22, 2021

--- script.R
+++ script.R
@@ -9,89 +9,66 @@ cv19_wiki <- read_html(cv19_url)
 
 # 3. Save the raw data of CV19 in the US in a table
 cv19_raw <- cv19_wiki %>% html_nodes('table') %>% .[[5]] %>% html_table(fill=TRUE)
+View(cv19_raw)
 
-# 4. Visible review the structure and content of the raw data table
+# 4. Remove some irrelevant rows (18:19, 81:82, 144:145, 208:209, 271:272, 334:335),
+#    Then reset the row index of the data table
+cv19_raw <- cv19_raw[-c(18:19, 81:82, 144:145, 208:209, 271:272, 334:335), ]
+rownames(cv19_raw) <- NULL
+
+# 5. Review the structure and content of the raw data table
 str(cv19_raw)
 View(cv19_raw)
 
-# 5. Save the data of confirmed, deaths, and recovered cases from the raw table
-# Set border of the states' data table (top, bottom [the last updated row], left, right) 
-# Row #133 corresponds with date 2020-06-25
-# Set order of the US' data table (top, bottom [the last updated row], left, right)
-t_us <- c(2,142,57,63)
-t_st <- c(2,142,1,56)
+# 6. Save the data of confirmed, deaths, and recovered cases from the raw table
+#    Set border of the states' data table (top, bottom [the last updated row], left, right) 
+#    Row #345 corresponds with date 22-Jan-21
+#    Set order of the US' data table (top, bottom [the last updated row], left, right)
+t_us <- c(2,345,57,64)
+t_st <- c(2,345,1,56)
 cv19_us <- cv19_raw[c(t_us[1]:t_us[2]), c(t_us[3]:t_us[4])]
 cv19_st <- cv19_raw[c(t_st[1]:t_st[2]), c(t_st[3]:t_st[4])]
+rownames(cv19_us) <- NULL
+rownames(cv19_st) <- NULL
 
-# 6. Visible review the structure and content of the saved data table
+# 7. Visible review the structure and content of the saved data table
 str(cv19_us)
 View(cv19_us)
 str(cv19_st)
 View(cv19_st)
 
-# 7. Remove some irrelevant rows (17, 18, 50, 51, 82, 83, 115, 116), then reset the row index of the data table
-cv19_us <- cv19_us[-c(17:18, 50:51, 82:83, 115:116), ]
-cv19_st <- cv19_st[-c(17:18, 50:51, 82:83, 115:116), ]
-rownames(cv19_us) <- NULL
-rownames(cv19_st) <- NULL
-
 # 8. Assign proper column names in 2 data tables
-colnames(cv19_us)[c(3, 5, 7)] <- c('Cumulated_Confirmed', 'Cumulated_Deaths', 'Cumulated_Recovered')
+colnames(cv19_us)[c(3, 5, 7, 8)] <- c('Cumulated_Confirmed', 'Cumulated_Deaths', 'Cumulated_Recovered', 'Active')
 colnames(cv19_st) <- c('Date','AK','AZ','CA','CO','HI','ID','MT','NM','NV','OR','UT','WA','WY','IA','IL','IN','KS',
                        'MI','MN','MO','ND','NE','OH','OK','SD','WI','AL','AR','FL','GA','KY','LA','MS','NC','SC','TN',
                        'TX','VA','WV','CT','DC','DE','MA','MD','ME','NH','NJ','NY','PA','RI','VT','GU','MP','PR','VI')
 
-# 9. Process missing data
-# *** Check unrepoted number in DC and DE
-cv19_st$DC
-cv19_st$DC[47]
-cv19_st$DE
-cv19_st$DE[66]
-cv19_st$KY
-cv19_st$KY[79]
-cv19_st$CT
-cv19_st$CT[80]
-# *** Fill DC unrepoted number with average of day before (94) and day after (91)
-cv19_st$DC[47] <- 92
-cv19_st$DE[66] <- 211
-cv19_st$KY[79] <- 176
-cv19_st$CT[80] <- 669
-# *** Double check DC column
-cv19_st$DC
-cv19_st$DE
-cv19_st$KY
-cv19_st$CT
-# Fill each of emtpy cells with 0
-col <- c(2:7)
+# 9. Fill each of emtpy cells with 0
+col <- c(2:8)
 cv19_us[ , col][cv19_us[ , col]==''] <- 0
 col <- c(2:56)
 cv19_st[ , col][cv19_st[ , col]==''] <- 0
 
 # 10. Test sum of a column, but it fails due to improper data type
 sum(cv19_us$Confirmed)
-sapply(cv19_us[1:4], class)
+sapply(cv19_us[1:8], class)
 
 # 11. Convert data in columns, except Date of both tables to number
-col <- c(2:7)
-cv19_us[ , col] <- apply(cv19_us[ , col], 2, function(x) as.numeric(x))
+library(stringr)    # str_remove_all (commas)
+col <- c(2:8)
+cv19_us[ , col] <- apply(cv19_us[ , col], 2, function(x) as.numeric(str_remove_all(x, '[,]')))
 col <- c(2:56)
-cv19_st[ , col] <- apply(cv19_st[ , col], 2, function(x) as.numeric(x))
+cv19_st[ , col] <- apply(cv19_st[ , col], 2, function(x) as.numeric(str_remove_all(x, '[,]')))
 
 # 12. Retest column data type
-sapply(cv19_us[1:4], class)
+sapply(cv19_us[1:8], class)
 str(cv19_st)
 sum(cv19_us$Deaths)
 sum(cv19_st$CA)
 
 # 13. Fix and format the Date column in proper form for manipulations
-d <- as.Date('01/21/2020', format='%m/%d/%Y')
-# Last update date
-ld <- t_us[2] + 14
-i <- c(0,3:5,9:12,15,25,30,35:ld)
-Date <- c(d + i)
-View(Date)
-cv19_us$Date <- Date
-cv19_st$Date <- Date
+cv19_us$Date <- as.Date(cv19_us$Date, format="%d-%b-%y")
+cv19_st$Date <- as.Date(cv19_st$Date, format="%d-%b-%y")
 
 # 14. Double check column data types for both tables
 str(cv19_us)
@@ -123,13 +100,13 @@ str(df_st)
 View(df_us)
 View(df_st)
 # Assign the last row of the data frame df_us / df_st
-lr <- 133
+lr <- 344
 
-# 2. Pie chart shows all cases ('Positive': p, 'Death': d, 'Recovered'; r)
+# 2. Pie chart shows all cases ('Active': p, 'Death': d, 'Recovered'; r)
 p <- sum(df_us$Cumulated_Recovered[lr])
 d <- sum(df_us$Cumulated_Deaths[lr])
-r <- sum(df_us$Cumulated_Confirmed[lr])
-case <- c('Recovered','Death', 'Positive')
+r <- sum(df_us$Active[lr])
+case <- c('Recovered','Death', 'Active')
 case_number <- c(p, d, r)
 pie <- data.frame(Case=case, Value=case_number)
 head(pie)
@@ -150,7 +127,7 @@ pie_theme <- theme( plot.title = element_text(hjust=0.5, color='#E95D0F', size=1
                     panel.border=element_blank(),
                     panel.grid.major=element_blank(),
                     panel.grid.minor=element_blank())
-palette_color <- c('Death'='#0D1B2E', 'Recovered'='#429890', 'Positive'='#B51C35')
+palette_color <- c('Death'='#0D1B2E', 'Recovered'='#429890', 'Active'='#B51C35')
 pie_chart <- ggplot(pie, aes(x='', y=Value, fill=Case)) +
   geom_bar(stat='identity', width=1) + coord_polar('y', start=0) +
   geom_text(aes(label=paste(round(Value/sum(Value)*100, 1),'%'), x=1.3),
@@ -160,33 +137,33 @@ pie_chart <- ggplot(pie, aes(x='', y=Value, fill=Case)) +
   pie_theme
 pie_chart
 
-# 2. Point charts show all the progression of cases ('Positive', 'Deaths' and 'Recovered'; r) over time
+# 2. Point charts show all the progression of cases ('Active', 'Deaths' and 'Recovered'; r) over time
 library(scales)
 library(plotly)
-subtitle_con <- paste('Total positive: ', prettyNum(sum(df_us$Confirmed), big.mark=','))
+subtitle_con <- paste('Total Active: ', prettyNum(df_us$Active[lr], big.mark=','))
 subtitle_dea <- paste('Total Deaths: ', prettyNum(sum(df_us$Deaths), big.mark=','))
 subtitle_rec <- paste('Total Recovered: ', prettyNum(sum(df_us$Recovered), big.mark=','))
-plot_con <- ggplot(df_us, aes(x=Date, y=Confirmed)) +
+plot_active <- ggplot(df_us, aes(x=Date, y=Active)) + scale_y_log10() +
   geom_point(color='#B51C35') +
   geom_smooth( color='#0D1B2E') +
   theme(axis.text.x=element_text(angle=45, hjust=1)) +
   xlab('') +
   scale_x_date(labels=date_format('%b %d')) +
-  ylab('Confirmed Positives') +
+  ylab('Number of Active Cases (log10)') +
   scale_y_continuous(labels=function(x) format(x, scientific=FALSE)) 
-ggplotly(plot_con) %>% layout(title=list(text=plot_title, y=1), font=list(family='sans serif', size=18, color='#E95D0F')) %>%
+ggplotly(plot_active) %>% layout(title=list(text=plot_title, y=1), font=list(family='sans serif', size=18, color='#E95D0F')) %>%
   add_annotations(text=subtitle_con, font=list(size=18, color='#0D1B2E'),
                   xref='paper', yref='paper', x=0.5, y=0.995, showarrow=FALSE)  %>% 
   add_annotations(text=plot_caption, font=list(family='sans serif', size=14, color='#B51C35'),
                   xref='paper', yref='paper', x=0, y=0, showarrow=FALSE)
-plot_dr <- ggplot(df_us, aes(x=Date)) + 
+plot_dr <- ggplot(df_us, aes(x=Date)) + scale_y_log10() +
   geom_point(aes(y=Deaths)) + 
   geom_smooth(aes(y=Deaths, color='Deaths')) +
   geom_point(aes(y=Recovered)) +
   geom_smooth(aes(y=Recovered, color='Recovered')) +
   scale_color_manual(name='', values=c('Deaths'='#B51C35', 'Recovered'='#429890')) +
   theme(axis.text.x=element_text(angle=45, hjust=1)) +
-  ylab('') +
+  ylab('Number of Cases (log10)') +
   xlab('') +
   scale_x_date(labels=date_format('%b %d'))
 ggplotly(plot_dr) %>% layout(title=list(text=plot_title, y=1), font=list(family='sans serif', size=18, color='#E95D0F')) %>%   
@@ -200,12 +177,12 @@ ggplotly(plot_dr) %>% layout(title=list(text=plot_title, y=1), font=list(family=
 # 3. Aggregrate data points by week to graph barchart of weekly number of cases
 library(reshape2)   # melt()
 library(lubridate)  # week()
-weekly_count <- aggregate(df_us[ , c(2,4,6)], by=list(Week=week(df_us$Date)), sum)
+weekly_count <- aggregate(df_us[ , c(4,6,8)], by=list(Week=week(df_us$Date)), sum)
 weekly_count <- melt(weekly_count, id.vars='Week', variable.name='Case', value.name='Count')
 View(weekly_count)
-star_week <- 9
-end_week <- 27
-plot_weekly <- ggplot(weekly_count, aes(x=Week, y=Count, fill=Case)) +
+star_week <- 0
+end_week <- 53
+plot_weekly <- ggplot(weekly_count, aes(x=Week, y=Count, fill=Case)) + 
   geom_bar(stat='identity', position='dodge') + 
   scale_x_continuous(breaks=seq(star_week, end_week + 1, 2), limits=c(star_week, end_week + 1), expand=c(0,0)) +
   scale_fill_manual(name='', values = c('#B51C35','#E95D0F', '#429890')) +
@@ -226,7 +203,7 @@ cal_theme <- theme( plot.title = element_text(hjust=0.5, color='#0D1B2E', size=1
                     axis.title.x=element_blank(),
                     axis.title.y=element_blank() )
 # Calenda for Confirmed cases
-cal_con <- ggplot_calendar_heatmap(df_us, 'Date', 'Confirmed') +
+cal_con <- ggplot_calendar_heatmap(df_us, 'Date', 'Active') +
   scale_fill_continuous(low='yellow', high='#B51C35') +
   labs(title=plot_title, subtitle=subtitle_con, caption=plot_caption) + cal_theme
 cal_con
@@ -243,7 +220,7 @@ cal_rec
 grid.arrange(cal_con, cal_dea, cal_rec, ncol=1)
 
 # 5. State-by-state distribution map of cases
-# Read latitude & longitude data for each state
+#    Read latitude & longitude data for each state
 df_state <- read.csv('statelatlong.csv')
 df_state <- df_state[order(df_state$State), ]
 rownames(df_state) <- NULL
